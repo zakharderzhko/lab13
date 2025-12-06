90e13e198bb9470b4071424be285d91e125e7c0f
Zakhar Derzhko bc2024-6 <zakhar.derzhko@lnu.edu.ua>
2025-12-06 23:22:55 +0200

    Додано код файлу FServ.cs (форма для введення параметрів фільтрування, сортування чи пошуку).

---
+++ FServ.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sklad
+{
+    public partial class FServ : Form
+    {
+        public FServ()
+        {
+            InitializeComponent();
+            FServTB.Text = Form1.GlStringParameter; // Якщо фільтр уже був, то відразу записуємо його у вікно введення
+        }
+
+        private void FservBOk_Click(object sender, EventArgs e)
+        {
+            Form1.GlStringParameter = FServTB.Text; // записуємо введений критерій у основну форму
+            Close(); // закриваємо форму Fserv, щоб далі продовжувалось виконання програми
+        }
+    }
+}
