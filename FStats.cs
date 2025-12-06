46be08f1a6af9782e69d8537eea17864b621e6ef
Zakhar Derzhko bc2024-6 <zakhar.derzhko@lnu.edu.ua>
2025-12-06 23:24:27 +0200

    Додано код файлу FStats.cs (форма для відображення статистики).

---
+++ FStats.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sklad
+{
+    public partial class FStats : Form
+    {
+        public FStats()
+        {
+            InitializeComponent();
+        }
+
+        public void SetDataTable(DataTable dt)
+        {
+            dgStats.DataSource = dt;
+            dgStats.AutoResizeColumns();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
